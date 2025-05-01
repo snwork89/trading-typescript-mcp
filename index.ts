02d5627fba99180a73462eb507e0862f54e42503
snwork <100206710+snwork89@users.noreply.github.com>
2025-05-01 11:43:55 +0530

    basic_tool_exposed_to_claude

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
-import { placeOrder } from "./zerodhatrade";
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
 
-placeOrder("INFY", 1, "BUY");
+// Create an MCP server
+const server = new McpServer({
+  name: "Addition Server",
+  version: "1.0.0"
+});
+
+// Add an addition tool
+server.tool("add",
+  { a: z.number(), b: z.number() },
+  async ({ a, b }) => ({
+    content: [{ type: "text", text: String(a + b) }]
+  })
+);
+
+// Add a dynamic greeting resource
+
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
